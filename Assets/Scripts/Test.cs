6d3a99af506e88ce595c20077d98cf951b4c47ba
Rasul Tataev <tataev.r@postuf.com>
2018-11-29 11:30:02 +0300

    Сменил ориентацию на портретную в iOS. Необходимо так же сменить и на андроиде

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,11 +7,13 @@ public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		print("Hello World !!!");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.touches.Length > 0) {
+			print(Input.touches);
+		}
 	}
 }
